25eb587125195c67411eb8bc078c95ecdbed11d0
Akhileshwar Ankireddy <akhileshwar.reddy@mountblue.tech>
2020-11-13 18:13:43 +0530

    Remove Hardcoded API Key

--- open-weather.rb
+++ open-weather.rb
@@ -1,17 +1,10 @@
 require 'net/http'
 require 'json'
 
-# API_KEY = "eed51248a56975eaac956ecf809799c5"
-# URL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-
-class OpenWeather
-	def initialize()
-		@api_key = "eed51248a56975eaac956ecf809799c5"
-	end
-	
+class OpenWeather	
 	def get_weather(city_name)
 		begin
-			uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&appid=#{@api_key}")
+			uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&appid=#{ENV['OPEN_WEATHER_API_KEY']}")
 			res = Net::HTTP.get_response(uri)
 			data = JSON.parse(res.body)
 			data.each do |key, value|
